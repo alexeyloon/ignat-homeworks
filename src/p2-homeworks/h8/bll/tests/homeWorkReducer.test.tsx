a849af3f0c81538795700f5864716a99713585ff
alexeyloon <alexeyloon921@gmail.com>
2022-11-03 11:59:08 +0300

    make h8

--- src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
+++ src/p2-homeworks/h8/bll/tests/homeWorkReducer.test.tsx
@@ -1,32 +1,51 @@
 import React from 'react'
 import {homeWorkReducer} from '../homeWorkReducer'
 
-let initialState: any[] // need to fix any
+export type UserType = {
+  _id: number
+  name: string
+  age: number
+}
+let initialState: UserType[] // need to fix any
 
 beforeEach(() => {
-    initialState = [
-        {_id: 0, name: 'Кот', age: 3},
-        {_id: 1, name: 'Александр', age: 66},
-        {_id: 2, name: 'Коля', age: 16},
-        {_id: 3, name: 'Виктор', age: 44},
-        {_id: 4, name: 'Дмитрий', age: 40},
-        {_id: 5, name: 'Ирина', age: 55},
-    ]
+  initialState = [
+    {_id: 0, name: 'Кот', age: 3},
+    {_id: 1, name: 'Александр', age: 66},
+    {_id: 2, name: 'Коля', age: 16},
+    {_id: 3, name: 'Виктор', age: 44},
+    {_id: 4, name: 'Дмитрий', age: 40},
+    {_id: 5, name: 'Ирина', age: 55},
+  ]
 })
 
 test('sort name up', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
+  const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'up'})
 
-    console.log(newState)
-    // expect(...).toBe(...)
+  console.log(newState)
+  expect(newState[0]._id).toBe(1)
+
+  // const result = [
+  //   1,
+  //   3,
+  //   4,
+  //   5,
+  //   2,
+  //   0,
+  // ].filter((el, i) => el === newState[i]._id)
+  //
+  // expect(result.length).toBe(newState.length)
+
 })
 test('sort name down', () => {
-    const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
+  const newState = homeWorkReducer(initialState, {type: 'sort', payload: 'down'})
 
+  expect(newState[0]._id).toBe(0)
 
 })
 test('check age 18', () => {
-    const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
+  const newState = homeWorkReducer(initialState, {type: 'check', payload: 18})
 
+  expect(newState.length).toBe(4)
 
 })
